dac6f646513a4f62eac04b5aaa4d4f738d4c0285
cliffmax85 <cliffmax85@gmail.com>
2022-04-26 15:07:09 -0700

    finished without commits oops

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
+import { asyncGetQuotes, thenGetQuotes, getTryQuotes, asyncFinallyGetTryQuotes, thenGetTryQuotes, thenFinallyGetQuote, thenAsyncGetQuotes, asyncThenGetQuotes} from "./services/promise-me";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  const handleClick = (callback) => {
+    const result = callback();
+    console.log(result);
+  };
+  return (
+    <>
+      <button onClick={() => handleClick(asyncGetQuotes)}>
+        Async Get Quotes
+      </button>
+      <button onClick={() => handleClick(thenGetQuotes)}>
+        Get Then Quotes  
+      </button>  
+      <button onClick={() => handleClick(getTryQuotes)}>
+        Get async try Quotes  
+      </button>  
+      <button onClick={() => handleClick(asyncFinallyGetTryQuotes)}>
+        Get async finally get try Quotes  
+      </button>  
+      <button onClick={() => handleClick(thenGetTryQuotes)}>
+        Get try Quotes with then   
+      </button>  
+      <button onClick={() => handleClick(thenFinallyGetQuote)}>
+        Get Quotes with then and finally  
+      </button>  
+      <button onClick={() => handleClick(thenAsyncGetQuotes)}>
+        Get Quotes with then and finally using another function
+      </button>  
+      <button onClick={() => handleClick(asyncThenGetQuotes)}>
+        Get Quotes with then and finally and catch
+      </button>  
+    </>
+  )
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,32 +1,89 @@
 /**
  * TODO: Exercise 1: use `fetch` & async/await to get data from an API
  */
+export async function asyncGetQuotes() {
+  const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes/1');
+  const result =  await res.json();
 
+  return { quote: result[0], totalResults: result.length };
+}
 /**
  * TODO: Exercise 2: use `fetch` & `.then` syntax to get the same data from the first exercise.
  */
+export function thenGetQuotes() {
+    const result = fetch('https://futuramaapi.herokuapp.com/api/quotes/1')
+      .then((res) => res.json())
+      .then((result) => ({ quote: result[0], totalResults: result.length }))
 
+      return result;
+}
 /**
  * TODO: Exercise 3: use `fetch`, async/await, and a try/catch statement to get data from an API and handle errors
  */
-
+export async function getTryQuotes() {
+  try {
+    const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes/1');
+    const result =  await res.json();
+  
+    return { quote: result[0], totalResults: result.length };
+  } catch (error) {
+    console.error(error);
+  }
+}
 /**
  * TODO: Exercise 4: use `fetch`, `.then`, and `.catch` to get the same data from exercise 3 while handling errors
  */
+export function thenGetTryQuotes() {
+  const result = fetch('https://futuramaapi.herokuapp.com/api/quotes/1')
+    .then((res) => res.json())
+    .then((result) => ({ quote: result[0], totalResults: result.length }))
+    .catch((error) => console.error('There was an error!'));
 
+    return result;
+}
 /**
  * TODO: Exercise 5: use `fetch`, async/await, and a try/catch/finally statement to get data from an API, handle errors, then console.log('All done!') upon completion
  */
-
+export async function asyncFinallyGetTryQuotes() {
+  try {
+    const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes/1');
+    const result =  await res.json();
+  
+    return { quote: result[0], totalResults: result.length };
+  } catch (error) {
+    console.error(error);
+  } finally {
+    console.log('All done');
+  }
+}
 /**
  * TODO: Exercise 6: use `fetch`, `.then`, `.catch`. and `.finally` to get the same data from exercise 5 while handling errors and calling console.log('All done!') upon completion
  */
+export function thenFinallyGetQuote() {
+  const result = fetch('https://futuramaapi.herokuapp.com/api/quotes/1')
+    .then((res) => res.json())
+    .then((result) => ({ quote: result[0], totalResults: result.length }))
+    .catch((error) => console.error('An error happened Sadge'))
+    .finally(() => console.log('Finally!'));
 
+    return result;
+}
 /**
  * TODO: Exercise 7: Call your function from exercise 7 using .then
  */
-
+export function thenAsyncGetQuotes() {
+  thenFinallyGetQuote().then((result) => 
+  console.log('USed the last function inside this one with this result', result));
+}
 /**
  * TODO: Exercise 8: Call your function from exercise 6 using async/await
  */
+export async function asyncThenGetQuotes() {
+  const result = await fetch('https://futuramaapi.herokuapp.com/api/quotes/1')
+    .then((res) => res.json())
+    .then((result) => ({ quote: result[0], totalResults: result.length}))
+    .catch((error) => console.error('oopie, there was an error Sadge'))
+    .finally(() => console.log('U R DUN'));
 
+    return result;
+}
